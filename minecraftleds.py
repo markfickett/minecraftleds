42e4aca20ce76581e5fb626d60c99f17ec467d82
Mark Fickett <mark.fickett@gmail.com>
2014-04-27 11:18:57 -0400

    sketch status fetcher and communication

---
+++ minecraftleds.py
@@ -0,0 +1,15 @@
+
+
+_KNOWN_PLAYERS = frozenset((
+  'CapybaraRancher',
+  'Wickedhound',
+  'krc23',
+  'markfickett',
+))
+_UNKNOWN_PLAYER = 'other'
+
+
+if __name__ == '__main__':
+  get a Minecraft server status object
+  update every N seconds
+  write true/false for every player name
