3928f43358703521c6ce18c59b7592e48929090d
Mark Fickett <mark.fickett@gmail.com>
2014-04-27 13:14:11 -0400

    write LED status

--- minecraftleds.ino
+++ minecraftleds.ino
@@ -1,5 +1,6 @@
 #include <DataReceiver.h>
-#include <string.h>
+#include <array>
+#include <string>
 
 #define NUM_KEYS 5
 
@@ -10,10 +11,21 @@ DataReceiver<NUM_KEYS> receiver;
  */
 class FigureLight {
   private:
-    const int pinR, pinGB;
+    const int pinRed, pinGreenBluePwm;
+    bool available, online;
   public:
-    FigureLight(int redPin, int greenBluePin) :
-        pinR(redPin), pinGB(greenBluePin) {}
+    FigureLight(int i_pinRed, int i_pinGreenBluePwm) :
+        pinRed(i_pinRed), pinGreenBluePwm(i_pinGreenBluePwm), online(false) {}
+
+    void setup() {
+      pinMode(pinRed, OUTPUT);
+      pinMode(pinGreenBluePwm, OUTPUT);
+    }
+
+    /** Sets whether the server is available. */
+    void setAvailable(bool i_available) {
+      available = i_available;
+    }
 
     /**
      * Updates whether the character is online.
@@ -21,27 +33,39 @@ class FigureLight {
      * @param value A null-terminated stringification of a Python boolean.
      */
     void operator()(const char* value) {
-      if (strcmp(TRUE_STRING, value) == 0) {
-      } else {
-      }
+      online = strcmp(TRUE_STRING, value) == 0;
+    }
+
+    /**
+     * Updates the output LEDs based on server availability and whether the
+     * player is online.
+     */
+    void update() {
+      digitalWrite(pinRed, online ? HIGH: LOW);
+      digitalWrite(pinGreenBluePwm, online ? HIGH: LOW);
     }
 };
 
-FigureLight capy(1, 2);
+#define NUM_FIGURES 5
+
+FigureLight capy(3, 4);
 void capyCb(size_t size, const char* value) { capy(value); }
-FigureLight wick(3, 4);
+FigureLight wick(6, 5);
 void wickCb(size_t size, const char* value) { wick(value); }
-FigureLight krc(5, 6);
+FigureLight krc(8, 9);
 void krcCb(size_t size, const char* value) { krc(value); }
-FigureLight mark(7, 8);
+FigureLight mark(13, 14);
 void markCb(size_t size, const char* value) { mark(value); }
-FigureLight other(9, 10);
+FigureLight other(16, 15);
 void otherCb(size_t size, const char* value) { other(value); }
+
+FigureLight* allFigures[] = {&capy, &wick, &krc, &mark, &other};
 
 /** The Minecraft server became available on the network (or went offline). */
 void availableCb(size_t size, const char* value) {
-  if (strcmp(TRUE_STRING, value) == 0) {
-  } else {
+  bool available = strcmp(TRUE_STRING, value) == 0;
+  for (int i = 0; i < NUM_FIGURES; i++) {
+    allFigures[i]->setAvailable(available);
   }
 }
 
@@ -58,4 +82,7 @@ void setup() {
 
 void loop() {
   receiver.readAndUpdate();
+  for (int i = 0; i < NUM_FIGURES; i++) {
+    allFigures[i]->update();
+  }
 }
