d04ff106974b207185db18fe961e692a3718d97a
Mark Fickett <mark.fickett@gmail.com>
2015-05-21 21:57:14 -0400

    Add logging in startup/shutdown and when state changes.

--- minecraftleds.py
+++ minecraftleds.py
@@ -1,4 +1,5 @@
 import sys
+import logging
 import os
 import time
 
@@ -24,13 +25,26 @@ _KNOWN_PLAYERS = frozenset((
 _UNKNOWN_PLAYER = 'other'
 _AVAILABLE = 'available'
 
+_SERVER_ADDRESS = 'naib'
+
 
 if __name__ == '__main__':
-  server = McServer('naib')
+  logging.basicConfig(
+    format='%(levelname)s %(asctime)s %(filename)s:%(lineno)s: %(message)s',
+    level=logging.DEBUG)
+  logging.info('Preparing to run LEDs.')
+
+  server = McServer(_SERVER_ADDRESS)
+  server.Update()
+  logging.info(
+      '%s is %savailable.', _SERVER_ADDRESS, '' if server.available else 'not')
+
   sender = Sender('/dev/ttyACM0', start_ready=True, read_timeout=0.05)
 
   with sender:
+    logging.info('Communicating with Arduino.')
     try:
+      last_status = None
       while True:
         time.sleep(1.0)
         server.Update()
@@ -40,9 +54,13 @@ if __name__ == '__main__':
         status[_UNKNOWN_PLAYER] = bool(
             server.player_names_sample - _KNOWN_PLAYERS)
         status[_AVAILABLE] = server.available
+        if status != last_status:
+          logging.info(status)
+          last_status = status
         sender.Send(**status)
         sender.ReadAndPrint()
     except KeyboardInterrupt:
+      logging.info('Clearing LEDs and exiting.')
       final_status = {name: False for name in _KNOWN_PLAYERS}
       final_status[_UNKNOWN_PLAYER] = False
       sender.Send(**final_status)
